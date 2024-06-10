c83edd445293d40b07b4e2bf550d97041626870e
Liam727 <161321478+Liam727@users.noreply.github.com>
2024-06-10 12:07:38 +0200

    added spinner to act 2

--- app/src/main/java/com/example/finalpracticum/MainActivity2.kt
+++ app/src/main/java/com/example/finalpracticum/MainActivity2.kt
@@ -3,18 +3,40 @@ package com.example.finalpracticum
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Spinner
 import android.widget.TextView
 
 class MainActivity2 : AppCompatActivity() {
+
+    enum class Day {
+        Sunny,
+        Cloudy,
+        Raining,
+        Windy,
+        Hot,
+        Cold
+    }
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
 
+
         val nxtBtn =findViewById<Button>(R.id.nxtBtn)
+        val typeOfDay=findViewById<Spinner>(R.id.typeOfDay)
+        val highestTemp=findViewById<EditText>(R.id.highestTemp)
+        val lowestTemp=findViewById<EditText>(R.id.lowestTemp)
+
+
+        typeOfDay.adapter= ArrayAdapter<MainActivity2.Day>(
+            this,
+            android.R.layout.simple_list_item_1,
+            Day.values()
+        )
 
         nxtBtn.setOnClickListener {
             val intent = Intent(this,MainActivity3::class.java)
