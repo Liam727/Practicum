8a42470323ec60794bc109bf8becfc391602e41d
Liam727 <161321478+Liam727@users.noreply.github.com>
2024-06-10 11:51:05 +0200

    Added buttons to link acts

--- app/src/main/java/com/example/finalpracticum/MainActivity2.kt
+++ app/src/main/java/com/example/finalpracticum/MainActivity2.kt
@@ -1,7 +1,11 @@
 package com.example.finalpracticum
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity2 : AppCompatActivity() {
 
@@ -9,5 +13,19 @@ class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+
+        val nxtBtn =findViewById<Button>(R.id.nxtBtn)
+
+        nxtBtn.setOnClickListener {
+            val intent = Intent(this,MainActivity3::class.java)
+            startActivity(intent)
+        }
+
+
+
     }
-}
+
+
+    }
+
+
--- app/src/main/java/com/example/finalpracticum/MainActivity3.kt
+++ app/src/main/java/com/example/finalpracticum/MainActivity3.kt
@@ -1,11 +1,22 @@
 package com.example.finalpracticum
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity3 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main3)
+        
+       val backBtn =findViewById<Button>(R.id.backBtn)
+
+        backBtn.setOnClickListener {
+            val intent = Intent(this,MainActivity2::class.java)
+            startActivity(intent)
+        }
+        
     }
 }
