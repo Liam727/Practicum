8573c251bdc5ffe066b17dacd60227dcb6c45f59
Liam727 <161321478+Liam727@users.noreply.github.com>
2024-06-10 09:37:21 +0200

    First commit added splashscreen

---
+++ app/src/main/java/com/example/finalpracticum/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.finalpracticum
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val introText=findViewById<TextView>(R.id.introText)
+        val exitBtn=findViewById<Button>(R.id.exitBtn)
+        val enterBtn=findViewById<Button>(R.id.enterBtn)
+        val nameText=findViewById<TextView>(R.id.nameText)
+
+        enterBtn.setOnClickListener {
+            val intent = Intent(this,MainActivity2::class.java)
+            startActivity(intent)
+        }
+        exitBtn.setOnClickListener {
+            val intent =Intent(this,MainActivity2::class.java)
+            startActivity(Intent())
+        }
+
+    }
+}
